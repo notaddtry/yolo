39acd0b2b100c7005c2806edfd9935d41b6c206a
notaddtry <33911781+notaddtry@users.noreply.github.com>
2018-02-18 23:48:40 +0300

    Create CALCULATOR.cpp

---
+++ CALCULATOR.cpp
@@ -0,0 +1,23 @@
+
+#include "CALCULATOR.h" 
+
+void printresult(double r) {
+	cout << r << endl;
+}
+
+double add(double a, double b) {
+	return a + b;
+}
+
+double substract(double a, double b) {
+	return a - b;
+}
+
+double multiply(double a, double b) {
+	return a * b;
+}
+
+double divide(double a, double b) {
+  if b!=0
+	return a / b;
+}
